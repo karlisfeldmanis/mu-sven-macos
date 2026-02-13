2c328b79e7c00454809ef3b1a11f6209178b38b0
Karlis Feldmanis <karlisfeldmanis@LATKARFELMAC.local>
2026-02-13 15:48:52 +0200

    Pure C++ project: Purge Godot legacy assets and documentation

--- addons/mu_tools/util/diagnostic_check.gd
+++
@@ -1,9 +0,0 @@
-extends SceneTree
-
-func _init():
-	var errs = PreflightValidator.run_full_check()
-	if errs > 0:
-		print("[DIAGNOSTIC] Found %d script errors." % errs)
-	else:
-		print("[DIAGNOSTIC] All scripts validated successfully.")
-	quit(errs)
---
+++ build/CMakeFiles/MuRemaster.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MuRemaster.dir/external/imgui/backends/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/MuRemaster.dir/external/imgui/backends/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/MuRemaster.dir/external/imgui/backends/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/MuRemaster.dir/external/imgui/backends/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/MuRemaster.dir/external/imgui/imgui.cpp.o"
+  "CMakeFiles/MuRemaster.dir/external/imgui/imgui.cpp.o.d"
+  "CMakeFiles/MuRemaster.dir/external/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/MuRemaster.dir/external/imgui/imgui_draw.cpp.o.d"
+  "CMakeFiles/MuRemaster.dir/external/imgui/imgui_tables.cpp.o"
+  "CMakeFiles/MuRemaster.dir/external/imgui/imgui_tables.cpp.o.d"
+  "CMakeFiles/MuRemaster.dir/external/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/MuRemaster.dir/external/imgui/imgui_widgets.cpp.o.d"
+  "CMakeFiles/MuRemaster.dir/src/Camera.cpp.o"
+  "CMakeFiles/MuRemaster.dir/src/Camera.cpp.o.d"
+  "CMakeFiles/MuRemaster.dir/src/Screenshot.cpp.o"
+  "CMakeFiles/MuRemaster.dir/src/Screenshot.cpp.o.d"
+  "CMakeFiles/MuRemaster.dir/src/Terrain.cpp.o"
+  "CMakeFiles/MuRemaster.dir/src/Terrain.cpp.o.d"
+  "CMakeFiles/MuRemaster.dir/src/TerrainParser.cpp.o"
+  "CMakeFiles/MuRemaster.dir/src/TerrainParser.cpp.o.d"
+  "CMakeFiles/MuRemaster.dir/src/TextureLoader.cpp.o"
+  "CMakeFiles/MuRemaster.dir/src/TextureLoader.cpp.o.d"
+  "CMakeFiles/MuRemaster.dir/src/main.cpp.o"
+  "CMakeFiles/MuRemaster.dir/src/main.cpp.o.d"
+  "MuRemaster"
+  "MuRemaster.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MuRemaster.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
